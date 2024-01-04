e5fccb9b11aa955529fa7a80f5406299813a47b9
YamazoeShintaro <ys37799665@gmail.com>
2024-01-05 00:07:31 +0900

    ✨ メニューをアイコンに変更した。

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,13 @@
 import './globals.css';
 import { Roboto_Slab, Noto_Serif } from 'next/font/google';
 import Link from 'next/link';
+import { Home, Timer, Settings } from "@mui/icons-material";
 
 const LogoNavFnt = Roboto_Slab({ subsets: ['latin'] });
 const MainFnt = Noto_Serif({ subsets: ['latin'] });
+const HomeIcon = Home;
+const TimerIcon = Timer;
+const SettingsIcon = Settings;
 
 export const metadata = {
   title: 'Commit Timer',
@@ -26,17 +30,17 @@ export default function RootLayout({ children }) {
           <ul style={{ backgroundColor: "#101841", fontSize: 15 }} className='flex justify-around fixed bottom-0 left-0 right-0 z-30 text-slate-50'>
             <li>
               <Link className='no-underline' href="/">
-                <p className="menu-button">Home</p>
+                <p className="menu-button"><HomeIcon /></p>
               </Link>
             </li>
             <li>
               <Link className='no-underline' href="/timer">
-                <p className="menu-button">Timer</p>
+                <p className="menu-button"><TimerIcon /></p>
               </Link>
             </li>
             <li>
               <Link className='no-underline' href="/setting">
-                <p className='menu-button'>Setting</p>
+                <p className='menu-button'><SettingsIcon /></p>
               </Link>
             </li>
           </ul>
